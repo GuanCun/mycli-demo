e6d65ecc186ff4d67033132ad52230a78a0f7018
GuanCun <linguancun@163.com>
2024-04-07 14:06:17 +0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# mycli-demo
+
+> A demo project to show how to use mycli
+
+## Install
+
+```bash
+npm install
+```
+
+## Usage
+
+```bash
+npm link
+mycli --help
+```
+
+### 2. Run the following command to generate a `mycli-demo` project:
+
+```bash
+cd src/demo
+mycli create <projectName>
+```
---
+++ bin/cli.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+const { Command } = require("commander");
+const inquirer = require("inquirer");
+const ora = require("ora");
+const download = require("download-git-repo");
+const { errLog, successLog } = require("../src/utils/log.js");
+
+const program = new Command();
+
+// console.log("i'm a cli");
+
+// 打印参数
+// console.log(process.argv);
+
+program
+  .command("create <projectName>")
+  .description("create a new project")
+  .alias("c")
+  .option("-r, --react", "react template")
+  .option("-v, --vue", "vue template")
+  .option("-v2, --vue2", "vue2 template")
+  .option("-v3, --vue3", "vue3 template")
+  .action((projectName, options) => {
+    console.log(111, projectName, options);
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "frameTemplate",
+          message: "请选择框架类型",
+          choices: ["Vue3", "Vue2", "React"],
+        },
+      ])
+      .then((answer) => {
+        console.log(answer);
+
+        const spinner = ora();
+        spinner.text = "正在下载模板...";
+        spinner.start();
+        download(
+          "direct:https://github.com/flippidippi/download-git-repo-fixture.git",
+          projectName,
+          { clone: true },
+          function (err) {
+            if (err) {
+              spinner.fail("模板下载失败");
+              errLog(err);
+            } else {
+              spinner.succeed("模板下载成功");
+              successLog("项目初始化完成");
+            }
+          }
+        );
+      });
+  });
+
+program.version("1.0.0").parse(process.argv);
